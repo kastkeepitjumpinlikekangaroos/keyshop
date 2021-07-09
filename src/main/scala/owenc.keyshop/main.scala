85a50a0988413baa9a22c9c1f1dffc1b7cb8daaa
Owen Christie <owen.christie@nytimes.com>
2021-07-09 14:47:36 -0400

    added akka

--- src/main/scala/owenc.keyshop/main.scala
+++ src/main/scala/owenc.keyshop/main.scala
@@ -1,8 +1,49 @@
 package owenc.keyshop.main
 
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.AbstractBehavior
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.Behaviors
+
 object Main {
     def main(args: Array[String]): Unit = {
-        println("Hello, world!")
-        args.foreach {println}
+        //println("Hello, world!")
+        //args.foreach {println}
+        val testSystem = ActorSystem(System(), "testSystem")
+        testSystem ! "start"
+    }
+}
+
+object PrintMyActorRefActor {
+  def apply(): Behavior[String] =
+    Behaviors.setup(context => new PrintMyActorRefActor(context))
+}
+
+class PrintMyActorRefActor(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+
+  override def onMessage(msg: String): Behavior[String] =
+    msg match {
+      case "printit" =>
+        val secondRef = context.spawn(Behaviors.empty[String], "second-actor")
+        println(s"Second: $secondRef")
+        this
+    }
+}
+
+object System {
+  def apply(): Behavior[String] =
+    Behaviors.setup(context => new System(context))
+
+}
+
+class System(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+  override def onMessage(msg: String): Behavior[String] =
+    msg match {
+      case "start" =>
+        val firstRef = context.spawn(PrintMyActorRefActor(), "first-actor")
+        println(s"First: $firstRef")
+        firstRef ! "printit"
+        this
     }
 }
